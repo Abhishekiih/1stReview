c51fd456326d1a83e456b25c9996ef6b3638f448
Abhishekiih <abhiibaghel1011@gmail.com>
2025-01-27 15:43:43 +0530

    Matrix Addition

---
+++ Desktop/1srreview/MatrixAddition.java
@@ -0,0 +1,20 @@
+public class MatrixAddition{
+	public static void main(String []args){
+		int[][] matrix1={{1,2,3},{4,5,6},{7,8,9}};
+		int row=3;
+		int col=3;
+		int[][] matrix2={{1,2,3},{4,5,6},{7,8,9}};
+		int[][] matrix3=new int[row][col];
+		for(int i=0;i<row;i++){
+			for(int j=0;j<col;j++){
+				matrix3[i][j]=matrix1[i][j]+matrix2[i][j];
+			}
+		}
+		for(int i=0;i<row;i++){
+			for(int j=0;j<col;j++){
+				System.out.print(matrix3[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+}
